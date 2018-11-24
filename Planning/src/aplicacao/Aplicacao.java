64ae47243a461026f90189a717e1c9eb8895225b
Cassio <cassio.thiago@sga.pucminas.br>
2018-11-24 18:44:50 -0200

    Codigo Comentado

---
+++ Planning/src/aplicacao/Aplicacao.java
@@ -0,0 +1,18 @@
+package aplicacao;
+
+public class Aplicacao {
+	
+	
+	static class Aviao {
+		int custo;
+		int id;
+
+
+		public Aviao(int custo, int id) {
+			this.custo = custo;
+			this.id = id;
+		}
+	}
+	
+	
+}
